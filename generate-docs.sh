98b20bcdda247eaa82d706d139ee0ae60d4588eb
Pedro Penedo <pedro@scandit.com>
2017-10-16 16:06:04 +0200

    Revert "Added screenshots and adapted the iOS dependencies, modified the generator-docs to include images in the build"
    This reverts commit 77e6015ac1a51fa395327b0fa60de621bc50a466.

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-#doxygen docs/doxygen.conf
-#cp -r docs/images public-docs/html/img
-
 doxygen docs/doxygen.conf
-rm -rf public-docs/html/img
-mkdir public-docs/html/img
-cp -r docs/images/* public-docs/html/img
+cp -r docs/images public-docs/html/img
