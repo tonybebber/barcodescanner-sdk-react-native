46c86768503e96cb2c40757bfe5504f035e2a413
Franciszek Gonciarz <franciszek@scandit.com>
2018-01-25 15:48:06 +0100

    Add `setOverlayProperty` method

---
+++ ios/ScanditBarcodeScanner/include/SBSOverlayController+Properties.h
@@ -0,0 +1,15 @@
+//
+//  SBSOverlayController+Properties.h
+//  ScanditBarcodeScanner
+//
+//  Created by Franciszek Gonciarz on 25.01.2018.
+//  Copyright © 2018 Scandit. All rights reserved.
+//
+
+#include "SBSOverlayController.h"
+
+@interface SBSOverlayController (Properties)
+
+- (void)setProperty:(NSString *)property toValue:(BOOL)value;
+
+@end
