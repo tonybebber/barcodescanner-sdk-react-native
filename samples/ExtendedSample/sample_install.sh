81aa6b59bcf8aef71f82ca5b285bca2892855609
Franciszek Gonciarz <franciszek@scandit.com>
2018-03-30 12:24:14 +0200

    Add install scripts

---
+++ samples/ExtendedSample/sample_install.sh
@@ -0,0 +1,6 @@
+echo "Preparing for install..."
+rm -rf node_modules
+rm -f yarn.lock
+rm -rf ../SimpleSample/node_modules
+rm -f ../SimpleSample/yarn.lock
+yarn install
---
+++ samples/SimpleSample/sample_install.sh
@@ -0,0 +1,6 @@
+echo "Preparing for install..."
+rm -rf node_modules
+rm -f yarn.lock
+rm -rf ../ExtendedSample/node_modules
+rm -f ../ExtendedSample/yarn.lock
+yarn install
