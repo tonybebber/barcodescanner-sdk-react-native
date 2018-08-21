d445515779c6452e9e27826001c55d0dd1ee905e
Tibor Molnár <fatalaa@hotmail.com>
2018-08-21 11:36:09 +0200

    Add a labeled switch which can take a function for value

--- samples/ExtendedSample/app/components/LabeledSwitch.js
+++ samples/ExtendedSample/app/components/LabeledSwitch.js
@@ -32,3 +32,29 @@ export default class LabeledSwitch extends Component {
     );
   }
 }
+
+export class LabeledSwitchWithFunctionValue extends Component {
+  render() {
+    return (
+      <View 
+      style={{flexDirection: 'row', 
+        justifyContent: 'space-between',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginTop: 5,
+        marginRight: 10,
+        marginLeft: 10,
+        marginBottom: 5}}>
+      <Text>
+        { this.props.label }
+      </Text>
+      <Switch
+        disabled= { this.props.disabled }
+        thumbTintColor='blue'
+        onTintColor='cornflowerblue'
+        value={ this.props.value() }
+        onValueChange={ this.props.listener }/>
+    </View>
+    )
+  }
+}
