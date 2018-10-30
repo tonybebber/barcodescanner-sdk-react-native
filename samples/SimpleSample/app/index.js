cce7ab8a6b6fc57c3aebb0936edb9cb1e5b715ca
Tomasz Jurkowski <tomasz@scandit.com>
2018-10-30 16:36:44 +0100

    Initial code for Android Camera handling for SimpleSample added.

--- samples/SimpleSample/app/index.js
+++ samples/SimpleSample/app/index.js
@@ -5,7 +5,10 @@ import {
   StyleSheet,
   Text,
   findNodeHandle,
-  View
+  View,
+  Platform,
+  PermissionsAndroid,
+  BackHandler
 } from 'react-native';
 import {
   BarcodePicker,
@@ -16,11 +19,12 @@ import {
   ScanSettings
 } from 'scandit-react-native';
 
-ScanditModule.setAppKey('-- ENTER YOUR SCANDIT LICENSE KEY HERE --');
+ScanditModule.setAppKey('AUSrZQgfFegTMaMl3UPk11YKZtj3KdetWX+nLv5jFRMEdXd3SkM0xi5uMs5JTR8xJl/FR2Vs7yCObka2yCEWaUVv/sZkc9OKzBgjasdRwmH7XkE0Sn5D/CNpJnXEd35fp2yQoQxzPfeCaG9HWUXwLvVW8aAzfib6G1ETKlFD0vGyVtEiPUrebqt8iem+cFxa10MwWfZg6Tp7bGq3XXRoaz50a1L5dtXdE3gRfrhOL6irauTmkXRiYkRWcON6aQCjsU7zNDpUijiBbCnKy2LPBtdccFtfQy2QYnS+FL0o9zF/d8WMOXDdI2Rcqe4pQU/1c0i4Ce4GpzNgC6XazhQjH6mFunp59E2nbOhbhZaTUZQm00SodYoX9iryvcYqjnepqT8WEGORPZzb2bry8t0X0HPNvlrnjSMuxnCjLl9n4CseLqisJVNlgJsWoSD67UbvFjMkNKXrZDLFHBhUh8tG9LIkVu/5f1gBJqtvOUQ4cKNHjH7yrTAMkk+P0XwupLHXVp/yDnafCDv8Tn1jAlC/+OpQsjeF7zkI1j7jOrV09IxWCT+eiGOo/9eGFYtAEo7KsQSHTvUECaSwwKd/gOZt4bh7SfDmNuIP10d5myF1rFYVbcOTeRVrmmr0PhrO3s4LyBRp8QK3GzSCs/Jp6TLfQCIBz3nHbW/wcVBmCyGTA8zOPzYuJFAXV/drQQUwpQavKiA1h8g71foX20Dn2bqYnAJK4goXu2bdK/9637MoLjsRTAZvT3pVrG/1aM2kkKPVgFQtDVcaQLdsHJuTyXX2M0ni40sTkCQN0YfiFjgAasnozw/5uUuDCzJVcNFq6JGdfM/zSNiK+EQaP/w2kLlf1gw1bg7iHw5CtQHqvKa2mOP0g3zntApB88ClErDwiYO4VmhQM20BalxtTbKkUhPE7lVz+e2/9z8yOPLKpIiQgqWo7xIauOjbBY3iUjgRYndtfeSM3hWXBW/1KKc8+a9ptrZPSWV+QsMNngID6rspuxsD7ScM+OH8GSperqPXGA==');
 
 export default class SimpleSample extends Component {
 
   componentWillMount() {
+    console.log("WTF componentWillMount");
     this.settings = new ScanSettings();
     this.settings.setSymbologyEnabled(Barcode.Symbology.EAN13, true);
     this.settings.setSymbologyEnabled(Barcode.Symbology.EAN8, true);
@@ -43,9 +47,51 @@ export default class SimpleSample extends Component {
        a look at http://docs.scandit.com/stable/c_api/symbologies.html. */
   }
 
-  componentDidMount() {
+  async hasCameraPermission() {
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA);
+      return granted;
+    } else {
+      return true;
+    }
+  }
+
+  async requestCameraPermission() {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log("Android Camera Permission has been granted.");
+          this.cameraPermissionGranted();
+        } else {
+          console.log("Android Camera Permission has been denied - the app will shut itself down.");
+          this.cameraPermissionDenied();
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    } else {
+      this.cameraPermissionGranted();
+    }
+  }
+
+  // This method should only be called if the Platform.OS is android.
+  cameraPermissionDenied() {
+    BackHandler.exitApp();
+  }
+
+  cameraPermissionGranted() {
     this.scanner.startScanning();
     AppState.addEventListener('change', this._handleAppStateChange);
+  }
+
+  async componentDidMount() {
+    const hasPermission = await this.hasCameraPermission();
+    if (hasPermission) {
+      this.cameraPermissionGranted();
+    } else {
+      await this.requestCameraPermission();
+    }
   }
   
   componentWillUnmount() {
@@ -63,14 +109,14 @@ export default class SimpleSample extends Component {
   render() {
     return (
       <View style={{
-			flex: 1,
-			flexDirection: 'column'}}>
-			<BarcodePicker
-				onScan={(session) => { this.onScan(session) }}
-				scanSettings= { this.settings }
-        ref={(scan) => { this.scanner = scan }}
-				style={{ flex: 1 }}/>
-    </View>
+        flex: 1,
+        flexDirection: 'column'}}>
+        <BarcodePicker
+          onScan={(session) => { this.onScan(session) }}
+          scanSettings= { this.settings }
+          ref={(scan) => { this.scanner = scan }}
+          style={{ flex: 1 }}/>
+      </View>
     );
   }
 
